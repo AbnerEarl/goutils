a6e18884d4226c9e0b7056300c69494e7083c326
yangchangjia <yangchangjia@qianxin.com>
2024-08-21 17:50:25 +0800

    add shorturl utils

--- web/autoid.go
+++ web/autoid.go
@@ -15,13 +15,8 @@ import (
 
 const (
 	MAX_RANDOM_NUMBER             = 256
-	MIN_RANDOM_NUMBER             = 0
 	SALT_ONE_LENGTH               = 8
 	SALT_TWO_LENGTH               = 8
-	INCREASE_COUNTER_LENGTH       = 47
-	HIGHEST_NUMBER                = "0"
-	BINARY_PREFIX                 = "0b"
-	UUID_NUMBER_LENGTH            = 63
 	SALT_ONE_SHIFT                = SALT_TWO_LENGTH
 	INCREASE_COUNTER_SHIFT        = SALT_TWO_LENGTH + SALT_ONE_LENGTH
 	AUTO_ID_GENERATOR_COUNTER_KEY = "auto_id_generator_counter_key"
@@ -63,6 +58,48 @@ func GenAutoIdsByClu(n uint64, client *redisc.RedisClusterCli) []uint64 {
 		saltOne, _ := rand.Int(rand.Reader, big.NewInt(MAX_RANDOM_NUMBER))
 		saltTwo, _ := rand.Int(rand.Reader, big.NewInt(MAX_RANDOM_NUMBER))
 		incrValue, _ := client.RdbIncr(AUTO_ID_GENERATOR_COUNTER_KEY)
+		id := (incrValue << INCREASE_COUNTER_SHIFT) | (saltOne.Int64() << SALT_ONE_SHIFT) | saltTwo.Int64()
+		result = append(result, uint64(id))
+	}
+	return result
+}
+
+func GenAutoIdByKey(client *redisc.RedisCli, counterGroupKey string) uint64 {
+	saltOne, _ := rand.Int(rand.Reader, big.NewInt(MAX_RANDOM_NUMBER))
+	saltTwo, _ := rand.Int(rand.Reader, big.NewInt(MAX_RANDOM_NUMBER))
+	incrValue, _ := client.RdbIncr(counterGroupKey)
+	result := (incrValue << INCREASE_COUNTER_SHIFT) | (saltOne.Int64() << SALT_ONE_SHIFT) | saltTwo.Int64()
+	return uint64(result)
+}
+
+func GenAutoIdsByKey(n uint64, client *redisc.RedisCli, counterGroupKey string) []uint64 {
+	result := []uint64{}
+
+	for i := uint64(0); i < n; i++ {
+		saltOne, _ := rand.Int(rand.Reader, big.NewInt(MAX_RANDOM_NUMBER))
+		saltTwo, _ := rand.Int(rand.Reader, big.NewInt(MAX_RANDOM_NUMBER))
+		incrValue, _ := client.RdbIncr(counterGroupKey)
+		id := (incrValue << INCREASE_COUNTER_SHIFT) | (saltOne.Int64() << SALT_ONE_SHIFT) | saltTwo.Int64()
+		result = append(result, uint64(id))
+	}
+	return result
+}
+
+func GenAutoIdByKeyClu(client *redisc.RedisClusterCli, counterGroupKey string) uint64 {
+	saltOne, _ := rand.Int(rand.Reader, big.NewInt(MAX_RANDOM_NUMBER))
+	saltTwo, _ := rand.Int(rand.Reader, big.NewInt(MAX_RANDOM_NUMBER))
+	incrValue, _ := client.RdbIncr(counterGroupKey)
+	result := (incrValue << INCREASE_COUNTER_SHIFT) | (saltOne.Int64() << SALT_ONE_SHIFT) | saltTwo.Int64()
+	return uint64(result)
+}
+
+func GenAutoIdsByKeyClu(n uint64, client *redisc.RedisClusterCli, counterGroupKey string) []uint64 {
+	result := []uint64{}
+
+	for i := uint64(0); i < n; i++ {
+		saltOne, _ := rand.Int(rand.Reader, big.NewInt(MAX_RANDOM_NUMBER))
+		saltTwo, _ := rand.Int(rand.Reader, big.NewInt(MAX_RANDOM_NUMBER))
+		incrValue, _ := client.RdbIncr(counterGroupKey)
 		id := (incrValue << INCREASE_COUNTER_SHIFT) | (saltOne.Int64() << SALT_ONE_SHIFT) | saltTwo.Int64()
 		result = append(result, uint64(id))
 	}
